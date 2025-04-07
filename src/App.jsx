c6621d972603fbe6ba3ab7de627f0ff7bede2834
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-04-07 22:54:48 +0600

    Assemble Your Ultimate Dream 11 Cricket Team  section has been added here static

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Header from './components/header/Header'
 import Navbar from './components/navbar/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Navbar></Navbar>
+     <Header></Header>
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='container mx-auto flex justify-between items-center mt-4 px-2'>
-            <img src="/src/assets/images/logo.svg" alt="" />
+        <div className='container mx-auto flex items-center mt-4 px-2'>
+            <div className='flex flex-1'>
+                <img src="/src/assets/images/logo.svg" alt="" />
+            </div>
             <ul className='hidden sm:flex gap-6 items-center'>
                 <li>Home</li>
                 <li>Fixture</li>
                 <li>Teams</li>
                 <li>Schedules</li>
-                <button className='flex border-1 px-2 py-1 rounded-sm'>0 Coin <img src="/src/assets/images/coin.svg" alt="" /></button>
             </ul>
-            <button className='sm:hidden flex border-1 px-2 py-1 rounded-sm'>0 Coin <img src="/src/assets/images/coin.svg" alt="" /></button>
+            <button className='flex border-1 ml-4 px-2 py-1 rounded-sm'>0 Coin <img src="/src/assets/images/coin.svg" alt="this is coin"/></button>
         </div>
     );
 };
